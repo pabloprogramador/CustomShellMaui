1bc8f8a9982ba4a706cce78127cf61db58267667
Pablo <pablo.erick@xpand-it.com>
2023-09-08 16:12:43 +0100

    fix android

--- CustomShellMaui.Sample/obj/Debug/net7.0-android/android/src/com/microsoft/maui/R.java
+++ CustomShellMaui.Sample/obj/Debug/net7.0-android/android/src/com/microsoft/maui/R.java
@@ -36,10 +36,10 @@ public final class R {
 		public static final int design_bottom_sheet_slide_out = 0x7f01001a;
 		public static final int design_snackbar_in = 0x7f01001b;
 		public static final int design_snackbar_out = 0x7f01001c;
-		public static final int fragment_fast_out_extra_slow_in = 0x7f010021;
-		public static final int mtrl_bottom_sheet_slide_in = 0x7f010026;
-		public static final int mtrl_bottom_sheet_slide_out = 0x7f010027;
-		public static final int mtrl_card_lowers_interpolator = 0x7f010028;
+		public static final int fragment_fast_out_extra_slow_in = 0x7f01002b;
+		public static final int mtrl_bottom_sheet_slide_in = 0x7f010030;
+		public static final int mtrl_bottom_sheet_slide_out = 0x7f010031;
+		public static final int mtrl_card_lowers_interpolator = 0x7f010032;
 	}
 	public static final class animator {
 		public static final int design_appbar_state_list_animator = 0x7f020000;
---
+++ CustomShellMaui.Sample/obj/Debug/net7.0-android/android/src/crc640482d5cad9e499cd/MainActivity.java
@@ -0,0 +1,48 @@
+package crc640482d5cad9e499cd;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"";
+		mono.android.Runtime.register ("CustomShellMaui.Sample.MainActivity, CustomShellMaui.Sample", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("CustomShellMaui.Sample.MainActivity, CustomShellMaui.Sample", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("CustomShellMaui.Sample.MainActivity, CustomShellMaui.Sample", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
