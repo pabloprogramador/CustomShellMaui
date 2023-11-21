5871281191576ac618b016b7e1bf07f3946a474a
Pablo <pablo.erick@xpand-it.com>
2023-11-21 13:09:08 +0000

    fix bug android

--- CustomShellMaui/obj/Release/net7.0-android/CustomShellMaui.AssemblyInfo.cs
+++ CustomShellMaui/obj/Release/net7.0-android/CustomShellMaui.AssemblyInfo.cs
@@ -14,11 +14,12 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Custom Shell Navigation with custom animation transition. A .NET MAUI library for" +
     " easily customizable animations of transitions of pages.")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.4.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.4")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.6.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.6")]
 [assembly: System.Reflection.AssemblyProductAttribute("CustomShellMaui")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CustomShellMaui")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.4.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.6.0")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/pabloprogramador/CustomShellMaui")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Android33.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Android21.0")]
 
